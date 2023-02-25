244d79f3a2343bdd64367a3cd8f26a3d5b338fc7
gregmika <grzegorz.mika@relia-sol.pl>
2023-02-25 23:30:05 +0100

    a lot of stuff

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"chat/internal/handlers"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 )
@@ -8,7 +9,11 @@ import (
 const webPort = ":8080"
 
 func main() {
+	log.SetReportCaller(true)
 	mux := routes()
+
+	log.Info("Starting channel listener")
+	go handlers.ListenToWsChannel()
 
 	log.WithField("port", webPort).Info("Starting web server")
 
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,11 +1,17 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/CloudyKit/jet/v6"
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"sort"
 )
+
+var wsChan = make(chan WsPayload)
+
+var clients = make(map[WebSocketConnection]string)
 
 var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./html"),
@@ -21,6 +27,8 @@ var upgradeConnection = websocket.Upgrader{
 }
 
 // Home renders the home page
+//
+//goland:noinspection GoUnusedParameter
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := renderPage(w, "home.jet", nil)
 	if err != nil {
@@ -28,11 +36,23 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
 // WsJsonResponse defines the response sent back from the websocket
 type WsJsonResponse struct {
-	Action      string `json:"action"`
-	Message     string `json:"message"`
-	MessageType string `json:"message_type"`
+	Action         string   `json:"action"`
+	Message        string   `json:"message"`
+	MessageType    string   `json:"message_type"`
+	ConnectedUsers []string `json:"connected_users"`
+}
+
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  string              `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
 }
 
 // WsEndpoint upgrades the connection to a websocket
@@ -47,9 +67,88 @@ func WsEndpoint(w http.ResponseWriter, r *http.Request) {
 	var response WsJsonResponse
 	response.Message = `<em><small>Connected to server</small></em>`
 
+	conn := WebSocketConnection{
+		Conn: ws,
+	}
+	clients[conn] = ""
+
 	err = ws.WriteJSON(response)
 	if err != nil {
 		log.Error(err)
+	}
+
+	go ListenForWs(&conn)
+
+}
+
+func ListenForWs(conn *WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Error("Recovering from error: ", r)
+		}
+	}()
+
+	var payload WsPayload
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// no payload
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+
+func ListenToWsChannel() {
+	var response WsJsonResponse
+
+	for {
+		event := <-wsChan
+
+		switch event.Action {
+		case "username":
+			// get a list of all users and send it back via broadcast
+			clients[event.Conn] = event.Username
+			response.Action = "list_users"
+			response.ConnectedUsers = getUserList()
+			broadcastToAll(response)
+
+		case "left":
+			// remove a user from the list of connected users
+			response.Action = "list_users"
+			delete(clients, event.Conn)
+			users := getUserList()
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>: %s", event.Username, event.Message)
+			broadcastToAll(response)
+		}
+	}
+}
+
+func getUserList() []string {
+	var userList []string
+	for _, u := range clients {
+		if u != "" {
+			userList = append(userList, u)
+		}
+	}
+	sort.Strings(userList)
+	return userList
+}
+
+func broadcastToAll(response WsJsonResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Info("Websocket error: ", err)
+			_ = client.Close()
+			delete(clients, client)
+		}
 	}
 }
 
